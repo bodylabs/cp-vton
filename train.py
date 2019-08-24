4f7c7b74c4308a461c8b417edfa1b490daf2cf28
Di <zhangd@amazon.com>
2019-08-24 15:07:43 -0700

    p2p disc, gradient penalty, relativistic gan loss

--- train.py
+++ train.py
@@ -13,6 +13,7 @@ from tensorboardX import SummaryWriter
 from visualization import board_add_image, board_add_images
 from p2p_discriminator import define_D
 
+lambda_gp = 10
 
 def get_opt():
     parser = argparse.ArgumentParser()
@@ -42,114 +43,140 @@ def get_opt():
     opt = parser.parse_args()
     return opt
 
-def train_gmm(opt, train_loader, model, board):
-    model.cuda()
-    model.train()
+# def train_gmm(opt, train_loader, model, board):
+#     model.cuda()
+#     model.train()
 
-    # criterion
-    criterionL1 = nn.L1Loss()
+#     # criterion
+#     criterionL1 = nn.L1Loss()
     
-    # optimizer
-    optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr, betas=(0.5, 0.999))
-    scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda = lambda step: 1.0 -
-            max(0, step - opt.keep_step) / float(opt.decay_step + 1))
+#     # optimizer
+#     optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr, betas=(0.5, 0.999))
+#     scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda = lambda step: 1.0 -
+#             max(0, step - opt.keep_step) / float(opt.decay_step + 1))
     
-    for step in range(opt.keep_step + opt.decay_step):
-        iter_start_time = time.time()
-        inputs = train_loader.next_batch()
+#     for step in range(opt.keep_step + opt.decay_step):
+#         iter_start_time = time.time()
+#         inputs = train_loader.next_batch()
             
-        im = inputs['image'].cuda()
-        im_pose = inputs['pose_image'].cuda()
-        im_h = inputs['head'].cuda()
-        shape = inputs['shape'].cuda()
-        agnostic = inputs['agnostic'].cuda()
-        c = inputs['cloth'].cuda()
-        cm = inputs['cloth_mask'].cuda()
-        im_c =  inputs['parse_cloth'].cuda()
-        im_g = inputs['grid_image'].cuda()
+#         im = inputs['image'].cuda()
+#         im_pose = inputs['pose_image'].cuda()
+#         im_h = inputs['head'].cuda()
+#         shape = inputs['shape'].cuda()
+#         agnostic = inputs['agnostic'].cuda()
+#         c = inputs['cloth'].cuda()
+#         cm = inputs['cloth_mask'].cuda()
+#         im_c =  inputs['parse_cloth'].cuda()
+#         im_g = inputs['grid_image'].cuda()
             
-        grid, theta = model(agnostic, c)
-        warped_cloth = F.grid_sample(c, grid, padding_mode='border')
-        warped_mask = F.grid_sample(cm, grid, padding_mode='zeros')
-        warped_grid = F.grid_sample(im_g, grid, padding_mode='zeros')
+#         grid, theta = model(agnostic, c)
+#         warped_cloth = F.grid_sample(c, grid, padding_mode='border')
+#         warped_mask = F.grid_sample(cm, grid, padding_mode='zeros')
+#         warped_grid = F.grid_sample(im_g, grid, padding_mode='zeros')
 
-        visuals = [ [im_h, shape, im_pose], 
-                   [c, warped_cloth, im_c], 
-                   [warped_grid, (warped_cloth+im)*0.5, im]]
+#         visuals = [ [im_h, shape, im_pose], 
+#                    [c, warped_cloth, im_c], 
+#                    [warped_grid, (warped_cloth+im)*0.5, im]]
         
-        loss = criterionL1(warped_cloth, im_c)    
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+#         loss = criterionL1(warped_cloth, im_c)    
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
             
-        if (step+1) % opt.display_count == 0:
-            board_add_images(board, 'combine', visuals, step+1)
-            board.add_scalar('metric', loss.item(), step+1)
-            t = time.time() - iter_start_time
-            print('step: %8d, time: %.3f, loss: %4f' % (step+1, t, loss.item()), flush=True)
+#         if (step+1) % opt.display_count == 0:
+#             board_add_images(board, 'combine', visuals, step+1)
+#             board.add_scalar('metric', loss.item(), step+1)
+#             t = time.time() - iter_start_time
+#             print('step: %8d, time: %.3f, loss: %4f' % (step+1, t, loss.item()), flush=True)
 
-        if (step+1) % opt.save_count == 0:
-            save_checkpoint(model, os.path.join(opt.checkpoint_dir, opt.name, 'step_%06d.pth' % (step+1)))
+#         if (step+1) % opt.save_count == 0:
+#             save_checkpoint(model, os.path.join(opt.checkpoint_dir, opt.name, 'step_%06d.pth' % (step+1)))
 
 
-def train_tom(opt, train_loader, model, board):
-    model.cuda()
-    model.train()
+# def train_tom(opt, train_loader, model, board):
+#     model.cuda()
+#     model.train()
     
-    # criterion
-    criterionL1 = nn.L1Loss()
-    criterionVGG = VGGLoss()
-    criterionMask = nn.L1Loss()
+#     # criterion
+#     criterionL1 = nn.L1Loss()
+#     criterionVGG = VGGLoss()
+#     criterionMask = nn.L1Loss()
     
-    # optimizer
-    optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr, betas=(0.5, 0.999))
-    scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda = lambda step: 1.0 -
-            max(0, step - opt.keep_step) / float(opt.decay_step + 1))
+#     # optimizer
+#     optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr, betas=(0.5, 0.999))
+#     scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda = lambda step: 1.0 -
+#             max(0, step - opt.keep_step) / float(opt.decay_step + 1))
     
-    for step in range(opt.keep_step + opt.decay_step):
-        iter_start_time = time.time()
-        inputs = train_loader.next_batch()
+#     for step in range(opt.keep_step + opt.decay_step):
+#         iter_start_time = time.time()
+#         inputs = train_loader.next_batch()
             
-        im = inputs['image'].cuda()
-        im_pose = inputs['pose_image']
-        im_h = inputs['head']
-        shape = inputs['shape']
+#         im = inputs['image'].cuda()
+#         im_pose = inputs['pose_image']
+#         im_h = inputs['head']
+#         shape = inputs['shape']
 
-        agnostic = inputs['agnostic'].cuda()
-        c = inputs['cloth'].cuda()
-        cm = inputs['cloth_mask'].cuda()
+#         agnostic = inputs['agnostic'].cuda()
+#         c = inputs['cloth'].cuda()
+#         cm = inputs['cloth_mask'].cuda()
         
-        outputs = model(torch.cat([agnostic, c],1))
-        p_rendered, m_composite = torch.split(outputs, 3,1)
-        p_rendered = F.tanh(p_rendered)
-        m_composite = F.sigmoid(m_composite)
-        p_tryon = c * m_composite+ p_rendered * (1 - m_composite)
+#         outputs = model(torch.cat([agnostic, c],1))
+#         p_rendered, m_composite = torch.split(outputs, 3,1)
+#         p_rendered = F.tanh(p_rendered)
+#         m_composite = F.sigmoid(m_composite)
+#         p_tryon = c * m_composite+ p_rendered * (1 - m_composite)
 
-        visuals = [ [im_h, shape, im_pose], 
-                   [c, cm*2-1, m_composite*2-1], 
-                   [p_rendered, p_tryon, im]]
+#         visuals = [ [im_h, shape, im_pose], 
+#                    [c, cm*2-1, m_composite*2-1], 
+#                    [p_rendered, p_tryon, im]]
             
-        loss_l1 = criterionL1(p_tryon, im)
-        loss_vgg = criterionVGG(p_tryon, im)
-        loss_mask = criterionMask(m_composite, cm)
-        loss = loss_l1 + loss_vgg + loss_mask
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+#         loss_l1 = criterionL1(p_tryon, im)
+#         loss_vgg = criterionVGG(p_tryon, im)
+#         loss_mask = criterionMask(m_composite, cm)
+#         loss = loss_l1 + loss_vgg + loss_mask
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
             
-        if (step+1) % opt.display_count == 0:
-            board_add_images(board, 'combine', visuals, step+1)
-            board.add_scalar('metric', loss.item(), step+1)
-            board.add_scalar('L1', loss_l1.item(), step+1)
-            board.add_scalar('VGG', loss_vgg.item(), step+1)
-            board.add_scalar('MaskL1', loss_mask.item(), step+1)
-            t = time.time() - iter_start_time
-            print('step: %8d, time: %.3f, loss: %.4f, l1: %.4f, vgg: %.4f, mask: %.4f' 
-                    % (step+1, t, loss.item(), loss_l1.item(), 
-                    loss_vgg.item(), loss_mask.item()), flush=True)
+#         if (step+1) % opt.display_count == 0:
+#             board_add_images(board, 'combine', visuals, step+1)
+#             board.add_scalar('metric', loss.item(), step+1)
+#             board.add_scalar('L1', loss_l1.item(), step+1)
+#             board.add_scalar('VGG', loss_vgg.item(), step+1)
+#             board.add_scalar('MaskL1', loss_mask.item(), step+1)
+#             t = time.time() - iter_start_time
+#             print('step: %8d, time: %.3f, loss: %.4f, l1: %.4f, vgg: %.4f, mask: %.4f' 
+#                     % (step+1, t, loss.item(), loss_l1.item(), 
+#                     loss_vgg.item(), loss_mask.item()), flush=True)
 
-        if (step+1) % opt.save_count == 0:
-            save_checkpoint(model, os.path.join(opt.checkpoint_dir, opt.name, 'step_%06d.pth' % (step+1)))
+#         if (step+1) % opt.save_count == 0:
+#             save_checkpoint(model, os.path.join(opt.checkpoint_dir, opt.name, 'step_%06d.pth' % (step+1)))
+
+
+def calc_gradient_penalty(netD, real_data, fake_data):
+    alpha = torch.rand(BATCH_SIZE, 1)
+    alpha = alpha.expand(BATCH_SIZE, int(real_data.nelement()/BATCH_SIZE)).contiguous()
+    alpha = alpha.view(BATCH_SIZE, 3, DIM, DIM)
+    alpha = alpha.to(device)
+    
+    fake_data = fake_data.view(BATCH_SIZE, 3, DIM, DIM)
+    interpolates = (alpha * real_data.detach() + ((1 - alpha) * fake_data.detach())).requires_grad_(True)
+
+    interpolates = interpolates.to(device)
+    interpolates.requires_grad_(True)
+
+    disc_interpolates = netD(interpolates)
+
+    gradients = autograd.grad(outputs=disc_interpolates, inputs=interpolates,
+                              grad_outputs=torch.ones(disc_interpolates.size()).to(device),
+                              create_graph=True, retain_graph=True, only_inputs=True)[0]
+
+    gradients = gradients.view(gradients.size(0), -1)                              
+    gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
+    return gradient_penalty
+
+
+
 
 
 ############### need to modify the cp_dataset.py before using
@@ -165,11 +192,13 @@ def train_wuton(opt, train_loader, model_gmm, model_tom, board):
     criterionL1 = nn.L1Loss()
     criterionVGG = VGGLoss()
 
-    netD = networks.define_D(opt.input_nc + opt.output_nc, opt.ndf, opt.netD,
-                                          opt.n_layers_D, opt.norm, opt.init_type, opt.init_gain, self.gpu_ids)
+    netD = networks.define_D(3, opt.ndf, opt.netD,
+                             opt.n_layers_D, opt.norm, opt.init_type, opt.init_gain, self.gpu_ids)
 
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr, betas=(0.5, 0.999))
+    optimizer_G = torch.optim.Adam(wuton.parameters(), lr=opt.lr, betas=(0.5, 0.999))
+    optimizer_D = torch.optim.Adam(netD.parameters(), lr=opt.lr, betas=(0.5, 0.999))
+
     scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda = lambda step: 1.0 -
             max(0, step - opt.keep_step) / float(opt.decay_step + 1))
 
@@ -197,8 +226,13 @@ def train_wuton(opt, train_loader, model_gmm, model_tom, board):
         grid_unpaired, theta_unpaired = model_gmm(torch.cat([agnostic, c_unpaired],1))
         outputs_unpaired = model_tom(torch.cat([agnostic, c_unpaired],1), theta)
 
+        y = torch.ones_like(outputs_unpaired.size()[0]) ########all 1
 
-
+        # ---------------------
+        #  Train Discriminator
+        # ---------------------
+        for p in wuton.parameters():
+            p.requires_grad_(False)  # freeze G
 
         # p_rendered, m_composite = torch.split(outputs, 3,3)
         # p_rendered = F.tanh(p_rendered)
@@ -209,46 +243,51 @@ def train_wuton(opt, train_loader, model_gmm, model_tom, board):
         #            [c, cm*2-1, m_composite*2-1], 
         #            [p_rendered, p_tryon, im]]
 
-        loss_warp_l1 = criterionL1(warped_cloth, im_c)    
-        loss_l1 = criterionL1(outputs, im)
-        loss_vgg = criterionVGG(outputs, im)
-
-        ######### loss_adv
-        y_pred = netD(im)
-
-        y_pred_fake_G = D(outputs_unpaired.detach()) # generator
-        y_pred_fake_D = D(outputs_unpaired) # discriminator
-        y = torch.ones_like(current_batch_size) ########all 1
-
         # Discriminator loss
-        loss_d = BCE_stable(y_pred - y_pred_fake_D, y)
+        optimizer_D.zero_grad()
+        y_pred = netD(im)
+        y_pred_fake_D = netD(outputs_unpaired) # discriminator
+        
 
-        # Generator loss (You may want to resample again from real and fake data)
-        loss_g = BCE_stable(y_pred_fake_G - y_pred, y)
+        gradient_penalty = calc_gradient_penalty(netD, im.data, outputs_unpaired.data)
 
-        loss_adv = loss_d + loss_g
+        loss_d = BCE_stable(y_pred - y_pred_fake_D, y) + lambda_gp * gradient_penalty
 
+        loss_d.backward()
+        optimizer_D.step()
 
+        # ---------------------
+        #  Train generator
+        # ---------------------
+        for p in netD.parameters():
+            p.requires_grad_(False)  # freeze D
 
+        if i % 5 == 0:
 
-        loss = loss_warp_l1 + loss_l1 + loss_vgg + loss_adv
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-            
-        # if (step+1) % opt.display_count == 0:
-        #     board_add_images(board, 'combine', visuals, step+1)
-        #     board.add_scalar('metric', loss.item(), step+1)
-        #     board.add_scalar('L1', loss_l1.item(), step+1)
-        #     board.add_scalar('VGG', loss_vgg.item(), step+1)
-        #     board.add_scalar('MaskL1', loss_mask.item(), step+1)
-        #     t = time.time() - iter_start_time
-        #     print('step: %8d, time: %.3f, loss: %.4f, l1: %.4f, vgg: %.4f, mask: %.4f' 
-        #             % (step+1, t, loss.item(), loss_l1.item(), 
-        #             loss_vgg.item(), loss_mask.item()), flush=True)
+            # Generator loss (You may want to resample again from real and fake data)
+            optimizer_G.zero_grad()
+            loss_warp_l1 = criterionL1(warped_cloth, im_c)    
+            loss_l1 = criterionL1(outputs, im)
+            loss_vgg = criterionVGG(outputs, im)
 
-        # if (step+1) % opt.save_count == 0:
-        #     save_checkpoint(model, os.path.join(opt.checkpoint_dir, opt.name, 'step_%06d.pth' % (step+1)))
+            y_pred_fake_G = netD(outputs_unpaired.detach()) # generator
+            loss_g = BCE_stable(y_pred_fake_G - y_pred, y) + loss_warp_l1 + loss_l1 + loss_vgg
+            loss_g.backward()
+            optimizer_G.step()
+                
+            # if (step+1) % opt.display_count == 0:
+            #     board_add_images(board, 'combine', visuals, step+1)
+            #     board.add_scalar('metric', loss.item(), step+1)
+            #     board.add_scalar('L1', loss_l1.item(), step+1)
+            #     board.add_scalar('VGG', loss_vgg.item(), step+1)
+            #     board.add_scalar('MaskL1', loss_mask.item(), step+1)
+            #     t = time.time() - iter_start_time
+            #     print('step: %8d, time: %.3f, loss: %.4f, l1: %.4f, vgg: %.4f, mask: %.4f' 
+            #             % (step+1, t, loss.item(), loss_l1.item(), 
+            #             loss_vgg.item(), loss_mask.item()), flush=True)
+
+            # if (step+1) % opt.save_count == 0:
+            #     save_checkpoint(model, os.path.join(opt.checkpoint_dir, opt.name, 'step_%06d.pth' % (step+1)))
 
 def main():
     opt = get_opt()
