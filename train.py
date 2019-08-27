c13a9dd5c229bf78bafd780a698b395e0380a898
Di <zhangd@amazon.com>
2019-08-27 14:59:35 -0700

    debug46

--- train.py
+++ train.py
@@ -312,7 +312,7 @@ def train_wuton(opt, train_loader, model_wuton, board):
             loss_l1 = criterionL1(outputs, im)
             loss_vgg = criterionVGG(outputs, im)
 
-            # y_pred_fake_G = netD(outputs_unpaired.detach()) # generator
+            # y_pred_fake_G = netD(outputs_unpaired) # generator
             # loss_g = BCE_stable(y_pred_fake_G - y_pred, y) + loss_warp_l1 + loss_l1 + loss_vgg
             loss_g = loss_warp_l1 + loss_l1 + loss_vgg
 
