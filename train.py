ce77ef2949855d23c07c731b195839f635bddd14
Di <zhangd@amazon.com>
2019-08-27 15:17:26 -0700

    debug55

--- train.py
+++ train.py
@@ -249,8 +249,8 @@ def train_wuton(opt, train_loader, model_wuton, board):
         # for p in netD.parameters():
         #     p.requires_grad_(True)  # reset D
 
-        # for p in model_wuton.parameters():
-        #     p.requires_grad_(False)  # freeze G
+        for p in model_wuton.parameters():
+            p.requires_grad_(False)  # freeze G
 
 
         #########paired
