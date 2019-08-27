64b8b075e72e70a1e6e4349cbb5d7ae0795f08fb
Di <zhangd@amazon.com>
2019-08-27 12:02:43 -0700

    debug13

--- train.py
+++ train.py
@@ -271,7 +271,7 @@ def train_wuton(opt, train_loader, model_gmm, model_tom, board):
 
         gradient_penalty = compute_gradient_penalty(netD, im.data, outputs_unpaired.data)
         loss_d = BCE_stable(y_pred - y_pred_fake_D, y) + lambda_gp * gradient_penalty
-        loss_d.backward()
+        loss_d.backward(retain_graph=True)
         optimizer_D.step()
 
         # ---------------------
@@ -293,7 +293,7 @@ def train_wuton(opt, train_loader, model_gmm, model_tom, board):
             if step != 200000:
                 loss_g.backward(retain_graph=True)
             else:
-                loss_g.backward()
+                loss_g.backward(retain_graph=True)
             optimizer_G.step()
                 
             if (step+1) % opt.display_count == 0:
