95fadb1ee151f1e333c8ece056d87fef52680f0a
Di <zhangd@amazon.com>
2019-08-27 10:27:41 -0700

    innermost

--- networks.py
+++ networks.py
@@ -403,7 +403,7 @@ class UnetSkipConnectionBlock(nn.Module):
             up_result = self.up(submodule_result)
             return up_result
         else:
-            if self.innermost:
+            if innermost:
                 xb,xc,xh,xw = input_c.size()
                 grid_function = TpsGridGen(xh, xw, use_cuda=True, grid_size=5)
                 grid = grid_function(theta)
