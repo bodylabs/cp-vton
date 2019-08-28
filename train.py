1e7e8c95aa7c03368792908ddd79b1ce35d0fffd
Di <zhangd@amazon.com>
2019-08-28 10:50:56 -0700

    modify unpairs

--- train.py
+++ train.py
@@ -48,137 +48,6 @@ def get_opt():
     opt = parser.parse_args()
     return opt
 
-# def train_gmm(opt, train_loader, model, board):
-#     model.cuda()
-#     model.train()
-
-#     # criterion
-#     criterionL1 = nn.L1Loss()
-    
-#     # optimizer
-#     optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr, betas=(0.5, 0.999))
-#     scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda = lambda step: 1.0 -
-#             max(0, step - opt.keep_step) / float(opt.decay_step + 1))
-    
-#     for step in range(opt.keep_step + opt.decay_step):
-#         iter_start_time = time.time()
-#         inputs = train_loader.next_batch()
-            
-#         im = inputs['image'].cuda()
-#         im_pose = inputs['pose_image'].cuda()
-#         im_h = inputs['head'].cuda()
-#         shape = inputs['shape'].cuda()
-#         agnostic = inputs['agnostic'].cuda()
-#         c = inputs['cloth'].cuda()
-#         cm = inputs['cloth_mask'].cuda()
-#         im_c =  inputs['parse_cloth'].cuda()
-#         im_g = inputs['grid_image'].cuda()
-            
-#         grid, theta = model(agnostic, c)
-#         warped_cloth = F.grid_sample(c, grid, padding_mode='border')
-#         warped_mask = F.grid_sample(cm, grid, padding_mode='zeros')
-#         warped_grid = F.grid_sample(im_g, grid, padding_mode='zeros')
-
-#         visuals = [ [im_h, shape, im_pose], 
-#                    [c, warped_cloth, im_c], 
-#                    [warped_grid, (warped_cloth+im)*0.5, im]]
-        
-#         loss = criterionL1(warped_cloth, im_c)    
-#         optimizer.zero_grad()
-#         loss.backward()
-#         optimizer.step()
-            
-#         if (step+1) % opt.display_count == 0:
-#             board_add_images(board, 'combine', visuals, step+1)
-#             board.add_scalar('metric', loss.item(), step+1)
-#             t = time.time() - iter_start_time
-#             print('step: %8d, time: %.3f, loss: %4f' % (step+1, t, loss.item()), flush=True)
-
-#         if (step+1) % opt.save_count == 0:
-#             save_checkpoint(model, os.path.join(opt.checkpoint_dir, opt.name, 'step_%06d.pth' % (step+1)))
-
-
-# def train_tom(opt, train_loader, model, board):
-#     model.cuda()
-#     model.train()
-    
-#     # criterion
-#     criterionL1 = nn.L1Loss()
-#     criterionVGG = VGGLoss()
-#     criterionMask = nn.L1Loss()
-    
-#     # optimizer
-#     optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr, betas=(0.5, 0.999))
-#     scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda = lambda step: 1.0 -
-#             max(0, step - opt.keep_step) / float(opt.decay_step + 1))
-    
-#     for step in range(opt.keep_step + opt.decay_step):
-#         iter_start_time = time.time()
-#         inputs = train_loader.next_batch()
-            
-#         im = inputs['image'].cuda()
-#         im_pose = inputs['pose_image']
-#         im_h = inputs['head']
-#         shape = inputs['shape']
-
-#         agnostic = inputs['agnostic'].cuda()
-#         c = inputs['cloth'].cuda()
-#         cm = inputs['cloth_mask'].cuda()
-        
-#         outputs = model(torch.cat([agnostic, c],1))
-#         p_rendered, m_composite = torch.split(outputs, 3,1)
-#         p_rendered = F.tanh(p_rendered)
-#         m_composite = F.sigmoid(m_composite)
-#         p_tryon = c * m_composite+ p_rendered * (1 - m_composite)
-
-#         visuals = [ [im_h, shape, im_pose], 
-#                    [c, cm*2-1, m_composite*2-1], 
-#                    [p_rendered, p_tryon, im]]
-            
-#         loss_l1 = criterionL1(p_tryon, im)
-#         loss_vgg = criterionVGG(p_tryon, im)
-#         loss_mask = criterionMask(m_composite, cm)
-#         loss = loss_l1 + loss_vgg + loss_mask
-#         optimizer.zero_grad()
-#         loss.backward()
-#         optimizer.step()
-            
-#         if (step+1) % opt.display_count == 0:
-#             board_add_images(board, 'combine', visuals, step+1)
-#             board.add_scalar('metric', loss.item(), step+1)
-#             board.add_scalar('L1', loss_l1.item(), step+1)
-#             board.add_scalar('VGG', loss_vgg.item(), step+1)
-#             board.add_scalar('MaskL1', loss_mask.item(), step+1)
-#             t = time.time() - iter_start_time
-#             print('step: %8d, time: %.3f, loss: %.4f, l1: %.4f, vgg: %.4f, mask: %.4f' 
-#                     % (step+1, t, loss.item(), loss_l1.item(), 
-#                     loss_vgg.item(), loss_mask.item()), flush=True)
-
-#         if (step+1) % opt.save_count == 0:
-#             save_checkpoint(model, os.path.join(opt.checkpoint_dir, opt.name, 'step_%06d.pth' % (step+1)))
-
-
-# def calc_gradient_penalty(netD, real_data, fake_data, BATCH_SIZE):
-#     alpha = torch.rand(BATCH_SIZE, 1)
-#     alpha = alpha.expand(BATCH_SIZE, int(real_data.nelement()/BATCH_SIZE)).contiguous()
-#     alpha = alpha.view(BATCH_SIZE, 3, DIM, DIM)
-#     alpha = alpha.to(device)
-    
-#     fake_data = fake_data.view(BATCH_SIZE, 3, DIM, DIM)
-#     interpolates = (alpha * real_data.detach() + ((1 - alpha) * fake_data.detach())).requires_grad_(True)
-
-#     interpolates = interpolates.to(device)
-#     interpolates.requires_grad_(True)
-
-#     disc_interpolates = netD(interpolates)
-
-#     gradients = autograd.grad(outputs=disc_interpolates, inputs=interpolates,
-#                               grad_outputs=torch.ones(disc_interpolates.size()).to(device),
-#                               create_graph=True, retain_graph=True, only_inputs=True)[0]
-
-#     gradients = gradients.view(gradients.size(0), -1)                              
-#     gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
-#     return gradient_penalty
 
 
 
@@ -253,23 +122,13 @@ def train_wuton(opt, train_loader, model_wuton, board):
         for p in model_wuton.parameters():
             p.requires_grad_(False)  # reset G
 
-        #########paired
-        outputs, grid, theta = model_wuton(c, dilated_upper_wuton)
-        warped_cloth = F.grid_sample(c, grid, padding_mode='border')
-        warped_grid = F.grid_sample(im_g, grid, padding_mode='zeros')
-        outputs = F.tanh(outputs)
         
         ########unpaired
-        outputs_unpaired, grid_unpaired, theta_unpaired = model_wuton(c, dilated_upper_wuton)
+        outputs_unpaired, grid_unpaired, theta_unpaired = model_wuton(c_unpaired, dilated_upper_wuton)
         outputs_unpaired = F.tanh(outputs_unpaired)
 
 
-        y = torch.ones(outputs.size()[0], 1, 6, 4).to(device) ########all 1
-
-
-        visuals = [[c, (warped_grid+warped_cloth)*0.5, im_c], 
-                   [dilated_upper_wuton, outputs, im]]
-
+        y = torch.ones(outputs_unpaired.size()[0], 1, 6, 4).to(device) ########all 1
 
 
         # Discriminator loss
@@ -309,6 +168,13 @@ def train_wuton(opt, train_loader, model_wuton, board):
                 p.requires_grad_(True)  # reset G
 
 
+            #########paired
+            outputs, grid, theta = model_wuton(c, dilated_upper_wuton)
+            warped_cloth = F.grid_sample(c, grid, padding_mode='border')
+            warped_grid = F.grid_sample(im_g, grid, padding_mode='zeros')
+            outputs = F.tanh(outputs)
+
+
             # Generator loss (You may want to resample again from real and fake data)
             optimizer_G.zero_grad()
             loss_warp_l1 = criterionL1(warped_cloth, im_c)    
@@ -316,12 +182,17 @@ def train_wuton(opt, train_loader, model_wuton, board):
             loss_vgg = criterionVGG(outputs, im)
 
 
-            outputs_unpaired_g, grid_unpaired, theta_unpaired = model_wuton(c, dilated_upper_wuton)
-            outputs_unpaired_g = F.tanh(outputs_unpaired_g)
-            y_pred_fake_G = netD(outputs_unpaired_g) # generator
-            y_pred_G = netD(im)
-            relativistic_loss_g = BCE_stable(y_pred_fake_G - y_pred_G, y)
+            # outputs_unpaired_g, grid_unpaired, theta_unpaired = model_wuton(c_unpaired, dilated_upper_wuton)
+            # outputs_unpaired_g = F.tanh(outputs_unpaired_g)
+            # y_pred_G = netD(im)
+            y_pred_fake_G = netD(outputs_unpaired) # generator
+            relativistic_loss_g = BCE_stable(y_pred_fake_G - y_pred, y)
             loss_g = relativistic_loss_g + loss_warp_l1 + loss_l1 + loss_vgg
+
+            visuals = [[c, (warped_grid+warped_cloth)*0.5, im_c], 
+                       [dilated_upper_wuton, outputs, im]]
+
+
 
 
             loss_g.backward()
