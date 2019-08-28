3d4d1159994f41f72ddac6bf794a1baa6cf1ed2d
Di <zhangd@amazon.com>
2019-08-27 18:09:43 -0700

    debug64

--- train.py
+++ train.py
@@ -246,8 +246,8 @@ def train_wuton(opt, train_loader, model_wuton, board):
         #  Train Discriminator
         # ---------------------
 
-        for p in netD.parameters():
-            p.requires_grad_(True)  # reset D
+        # for p in netD.parameters():
+        #     p.requires_grad_(True)  # reset D
 
         #########paired
         outputs, grid, theta = model_wuton(c, dilated_upper_wuton)
