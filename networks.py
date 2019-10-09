adf8c9bbdbb8b210166750d56726378cef6a47a8
Di <zhangd@amazon.com>
2019-10-08 22:27:31 -0700

    mix upsample and deconv 2

--- networks.py
+++ networks.py
@@ -357,7 +357,7 @@ class UnetSkipConnectionBlock(nn.Module):
         elif innermost:
             upsample = nn.Upsample(scale_factor=2, mode='bilinear')
             standard_upconv = [nn.Conv2d(inner_nc*2, inner_nc*2, kernel_size=3, stride=1, padding=1, bias=use_bias), nn.ReLU(True), norm_layer(inner_nc*2)]
-            upconv = [nn.ConvTranspose2d(inner_nc*2, outer_nc, kernel_size=4, stride=2, padding=1, bias=use_bias), n.ReLU(True), norm_layer(outer_nc)]
+            upconv = [nn.ConvTranspose2d(inner_nc*2, outer_nc, kernel_size=4, stride=2, padding=1, bias=use_bias), nn.ReLU(True), norm_layer(outer_nc)]
             
             down = [nn.Conv2d(input_nc, input_nc, kernel_size=3, stride=1, padding=1, bias=use_bias), nn.ReLU(True), nn.Conv2d(input_nc, inner_nc, kernel_size=4, stride=2, padding=1, bias=use_bias), nn.ReLU(True)]
             down_2 = [nn.Conv2d(input_nc, input_nc, kernel_size=3, stride=1, padding=1, bias=use_bias), nn.ReLU(True), nn.Conv2d(input_nc, inner_nc, kernel_size=4, stride=2, padding=1, bias=use_bias), nn.ReLU(True)]
