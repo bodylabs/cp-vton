5aeac71cd1b1938bbe07355c033a097bb9be1cd5
Di <zhangd@amazon.com>
2019-10-21 22:59:00 -0700

    remove batch norm

--- p2p_discriminator.py
+++ p2p_discriminator.py
@@ -252,7 +252,7 @@ class NLayerDiscriminator(nn.Module):
             sequence += [
                 nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult,
                           kernel_size=kw, stride=2, padding=padw, bias=use_bias),
-                norm_layer(ndf * nf_mult),
+                # norm_layer(ndf * nf_mult),
                 nn.LeakyReLU(0.2, True)
             ]
 
@@ -261,7 +261,7 @@ class NLayerDiscriminator(nn.Module):
         sequence += [
             nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult,
                       kernel_size=kw, stride=1, padding=padw, bias=use_bias),
-            norm_layer(ndf * nf_mult),
+            # norm_layer(ndf * nf_mult),
             nn.LeakyReLU(0.2, True)
         ]
 
